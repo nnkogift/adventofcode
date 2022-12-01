40ff7195a0d5a8d2861dba7f01248eb5b6647f42
gnnko <nnkogift@hotmail.com>
2022-12-01 16:06:21 +0300

    refactor(day1): Improvements
    - Refactored function names

--- src/day1/index.ts
+++ src/day1/index.ts
@@ -1,37 +1,38 @@
-// const input = "1000\n" +
-//     "2000\n" +
-//     "3000\n" +
-//     "\n" +
-//     "4000\n" +
-//     "\n" +
-//     "5000\n" +
-//     "6000\n" +
-//     "\n" +
-//     "7000\n" +
-//     "8000\n" +
-//     "9000\n" +
-//     "\n" +
-//     "10000"
+const input = "1000\n" +
+    "2000\n" +
+    "3000\n" +
+    "\n" +
+    "4000\n" +
+    "\n" +
+    "5000\n" +
+    "6000\n" +
+    "\n" +
+    "7000\n" +
+    "8000\n" +
+    "9000\n" +
+    "\n" +
+    "10000"
 
 
-import {input} from "./input";
+// import {input} from "./input";
 
-function runA() {
-    const elfs = input.split("\n\n");
+function getElfWithMostCaloriesCount() {
+    const elves = input.split("\n\n");
 
-    const elfCalories = elfs.map(elf => elf.split("\n"));
+    const elfCalories = elves.map(elf => elf.split("\n"));
     const caloriesSum = elfCalories.map(calories => calories.reduce((acc, calorie) => parseInt(calorie) + acc, 0));
     return caloriesSum.reduce((ans, value) => value > ans ? value : ans)
 }
 
-function runB() {
-    const elfs = input.split("\n\n");
+function getSumOfThreeElvesWithMostCaloriesCount() {
+    const elves = input.split("\n\n");
 
-    const elfCalories = elfs.map(elf => elf.split("\n"));
+    const elfCalories = elves.map(elf => elf.split("\n"));
     const caloriesSum = elfCalories.map(calories => calories.reduce((acc, calorie) => parseInt(calorie) + acc, 0));
     const sortedSum = caloriesSum.sort((a, b) => a > b ? 1 : -1).reverse();
 
     return sortedSum.slice(0, 3).reduce((acc, value) => value + acc, 0);
 }
 
-console.log(runB())
+console.log(getElfWithMostCaloriesCount())
+console.log(getSumOfThreeElvesWithMostCaloriesCount())
