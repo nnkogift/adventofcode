b2575643c951468891e4f11b1ee0b0975e99139e
gnnko <nnkogift@hotmail.com>
2022-12-01 15:25:53 +0300

    Initial commit

---
+++ src/day1/index.ts
@@ -0,0 +1,37 @@
+// const input = "1000\n" +
+//     "2000\n" +
+//     "3000\n" +
+//     "\n" +
+//     "4000\n" +
+//     "\n" +
+//     "5000\n" +
+//     "6000\n" +
+//     "\n" +
+//     "7000\n" +
+//     "8000\n" +
+//     "9000\n" +
+//     "\n" +
+//     "10000"
+
+
+import {input} from "./input";
+
+function runA() {
+    const elfs = input.split("\n\n");
+
+    const elfCalories = elfs.map(elf => elf.split("\n"));
+    const caloriesSum = elfCalories.map(calories => calories.reduce((acc, calorie) => parseInt(calorie) + acc, 0));
+    return caloriesSum.reduce((ans, value) => value > ans ? value : ans)
+}
+
+function runB() {
+    const elfs = input.split("\n\n");
+
+    const elfCalories = elfs.map(elf => elf.split("\n"));
+    const caloriesSum = elfCalories.map(calories => calories.reduce((acc, calorie) => parseInt(calorie) + acc, 0));
+    const sortedSum = caloriesSum.sort((a, b) => a > b ? 1 : -1).reverse();
+
+    return sortedSum.slice(0, 3).reduce((acc, value) => value + acc, 0);
+}
+
+console.log(runB())
